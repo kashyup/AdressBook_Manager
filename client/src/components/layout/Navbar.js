7a13e35126b6156e8c40841ab180d1d835d5cd26
kashyup <rohitkashyap69489@gmail.com>
2022-06-16 16:04:53 +0530

    nav bar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -21,7 +21,7 @@ const Navbar = ({ title, icon }) => {
 			<li>{user?.name}</li>
 			<li>
 				<a onClick={onLogout} href="#!">
-					<i className="fas fa-sign-out-alt"></i>{" "}
+					
 					<span className="hide-sm">Logout</span>
 				</a>
 			</li>
